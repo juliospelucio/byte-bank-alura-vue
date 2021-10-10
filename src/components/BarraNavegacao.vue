2ba37e8e86e5a74500decaaf9f47e902fec1f959
Julio Pelucio <juliopeluciofox@hotmail.com>
2021-10-10 20:07:55 -0300

    feat(navbar): Adding dynamic navbar.

---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-bytebank">
+    <a class="navbar-brand" href="#">ByteBank</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <BarraNavegacaoQuandoLogado v-if="usuarioestaLogado" />
+      <BarraNavegacaoQuandoDeslogado v-else />
+    </div>
+  </nav>
+</template>
+ 
+<script>
+import BarraNavegacaoQuandoLogado from "./BarraNavegacaoQuandoLogado";
+import BarraNavegacaoQuandoDeslogado from "./BarraNavegacaoQuandoDeslogado";
+export default {
+  components: {
+    BarraNavegacaoQuandoLogado,
+    BarraNavegacaoQuandoDeslogado,
+  },
+  computed: {
+    usuarioestaLogado() {
+      return Boolean(localStorage.getItem("token"));
+    },
+  },
+};
+</script>
+
+<style>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
---
+++ src/components/BarraNavegacaoQuandoDeslogado.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/login">Login</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/cadastre-se" class="nav-link">
+        Registre-se
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  methods: {
+    efetuarLogout() {
+      localStorage.removeItem("token");
+      this.$router.push({ name: "login" });
+    },
+  },
+};
+</script>
