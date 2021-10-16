239bf219f87975d5a99bc1949ddd2213e3916113
Julio Pelucio <juliopeluciofox@hotmail.com>
2021-10-16 16:11:13 -0300

    feat(guards): Protecting routes and defining public routes.

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,50 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
-import NovoUsuario from '../views/NovoUsuario.vue'
 import Login from '../views/Login.vue'
+
+import provedor from '@/provedor'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/gerentes',
-    name: 'gerentes',
-    component: Gerentes
-  },
-  {
-    path: '/cadastre-se',
-    name: 'novo.usuario',
-    component: NovoUsuario
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  }
+const routes = [{
+        path: '',
+        name: 'home',
+        component: Home,
+    },
+    {
+        path: '/gerentes',
+        name: 'gerentes',
+        component: Gerentes
+    },
+    {
+        path: '/cadastre-se',
+        name: 'novo.usuario',
+        component: () =>
+            import ( /*webpackChunkName: "registrar" */ '../views/NovoUsuario.vue'),
+        meta: {
+            publica: true
+        }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            publica: true
+        }
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+    if (!routeTo.meta.publica && !provedor.state.token) {
+        return next({ path: '/login' })
+    }
+    next()
 })
 
 export default router
